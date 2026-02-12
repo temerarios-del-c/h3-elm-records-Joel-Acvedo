51b4e4f599395d2592882a91ca9ddfbfc211a144
Joel-Acvedo~ <up230571@alumnos.upa.edu.mx>
2026-02-12 17:57:22 -0600

    1er ejercicio

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,4 +1,19 @@
 module Helper exposing (..)
 
 
-a : Int
+type alias Lenguaje =
+    { name : String, releaseYear : Int, currentVersion : String }
+
+
+langs : List Lenguaje
+langs =
+    [ { name = "Elm"
+      , releaseYear = 2012
+      , currentVersion = "0.19.1"
+      }
+    ]
+
+
+languageNames : List Lenguaje -> List String
+languageNames lista =
+    List.map .name lista
