4510612b284962823ca2ac1a51037509b49464f4
Joel-Acvedo~ <up230571@alumnos.upa.edu.mx>
2026-02-13 15:57:18 -0600

    Primer prueba

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,5 +1,35 @@
 module Helper exposing (..)
 
+--Ejercicio Simple 1
+
+
+add2 : Int -> Int -> Int
+add2 int1 int2 =
+    int1 + int2
+
+
+
+--Ejercicio Simple 2
+
+
+add3 : Float -> Float -> Float -> Float
+add3 int1 int2 int3 =
+    int1 + int2 + int3
+
+
+
+--Ejercicio simple 3
+
+
+calc : Int -> Int -> (Int -> Int -> Int) -> Int
+calc int1 int2 operator =
+    operator int1 int2
+
+
+
+-- Ejercicios de Records
+--Mostrar solo el nombre de una lista de records , es decir el nombre del lenguaje
+
 
 type alias Lenguaje =
     { name : String, releaseYear : Int, currentVersion : String }
@@ -7,9 +37,13 @@ type alias Lenguaje =
 
 langs : List Lenguaje
 langs =
-    [ { name = "Elm"
+    [ { name = "elm"
       , releaseYear = 2012
       , currentVersion = "0.19.1"
+      }
+    , { name = "javascript"
+      , releaseYear = 1995
+      , currentVersion = "ECMAScript 2025"
       }
     ]
 
@@ -17,3 +51,77 @@ langs =
 languageNames : List Lenguaje -> List String
 languageNames lista =
     List.map .name lista
+
+
+
+--Ejercicio 2 de records
+--Hacer un ejercicio que muestre si es Usuario
+--Nota para mi , un record puede servirte muchisimo , se llama como una variable de un objeto en java
+--user.name o user.edad o user.tipo
+--Puede ser con el nombre de la variable que yo quiera y puedo ponerla por delante
+--.uType perro
+
+
+type alias Usuario =
+    { name : String, uType : String }
+
+
+listaUniversidad : List Usuario
+listaUniversidad =
+    [ { name = "Roberto"
+      , uType = "Student"
+      }
+    , { name = "Mitsiu"
+      , uType = "Professor"
+      }
+    ]
+
+
+isStudent : Usuario -> String
+isStudent user =
+    if user.uType == "Student" then
+        user.name
+
+    else
+        ""
+
+
+onlyStudents : List Usuario -> List String
+onlyStudents lista =
+    List.map isStudent lista
+
+
+
+--Ejercicio 3 de Records
+--Definir un record para videojuegos
+
+
+type alias Videogame =
+    { title : String
+    , releaseYear : Int
+    , available : Bool
+    , downloads : Int
+    , genres : List String
+    }
+
+
+listVideogames : List Videogame
+listVideogames =
+    [ { title = "Control"
+      , releaseYear = 2019
+      , available = True
+      , downloads = 100
+      , genres = [ "Action", "Shooter" ]
+      }
+    , { title = "Ocarina of time"
+      , releaseYear = 2019
+      , available = True
+      , downloads = 100
+      , genres = [ "Action", "Adventure" ]
+      }
+    ]
+
+
+getVideogameGenres : List Videogame -> List (List String)
+getVideogameGenres videojuego =
+    List.map .genres videojuego
---
+++ src/Main.elm
@@ -0,0 +1,46 @@
+module Main exposing (..)
+
+import Html
+import Html.Attributes
+
+
+type alias Computer =
+    { ram : String
+    , model : String
+    , brand : String
+    , screenSize : String
+    }
+
+
+myLaptop : Computer
+myLaptop =
+    { ram = "16 GB"
+    , model = "Pavilon"
+    , brand = "HP"
+    , screenSize = "12 pulgadas"
+    }
+
+
+main : Html.Html msg
+main =
+    Html.div
+        []
+        [ Html.h1
+            []
+            [ Html.text "My laptop" ]
+        , Html.ul
+            []
+            [ Html.li
+                []
+                [ Html.text ("Ram: " ++ .ram myLaptop ++ "\"") ]
+            , Html.li
+                []
+                [ Html.text ("Modelo: " ++ .model myLaptop) ]
+            , Html.li
+                []
+                [ Html.text ("Marca: " ++ .brand myLaptop) ]
+            , Html.li
+                []
+                [ Html.text ("Pulgadas: " ++ .screenSize myLaptop) ]
+            ]
+        ]
