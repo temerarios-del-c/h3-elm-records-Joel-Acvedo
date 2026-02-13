92323a8d0fe30d9e024da9c048c21d7458601480
Joel-Acvedo~ <up230571@alumnos.upa.edu.mx>
2026-02-13 16:08:25 -0600

    Ya se que tuve mal XD

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,5 +1,10 @@
 module Helper exposing (..)
 
+import Html
+import Html.Attributes
+
+
+
 --Ejercicio Simple 1
 
 
@@ -125,3 +130,48 @@ listVideogames =
 getVideogameGenres : List Videogame -> List (List String)
 getVideogameGenres videojuego =
     List.map .genres videojuego
+
+
+type alias Computer =
+    { ram : String
+    , model : String
+    , brand : String
+    , screenSize : String
+    }
+
+
+myLaptop : Computer
+myLaptop =
+    { ram = "16 GB"
+    , model = "Pavilon"
+    , brand = "HP"
+    , screenSize = "12 pulgadas"
+    }
+
+
+main : Html.Html msg
+main =
+    Html.div
+        []
+        [ Html.h1
+            []
+            [ Html.text "My laptop" ]
+        , Html.div
+            []
+            [ Html.ul
+                []
+                [ Html.li
+                    []
+                    [ Html.text ("Ram: " ++ .ram myLaptop) ]
+                , Html.li
+                    []
+                    [ Html.text ("Modelo: " ++ .model myLaptop) ]
+                , Html.li
+                    []
+                    [ Html.text ("Marca: " ++ .brand myLaptop) ]
+                , Html.li
+                    []
+                    [ Html.text ("Pulgadas: " ++ .screenSize myLaptop) ]
+                ]
+            ]
+        ]
